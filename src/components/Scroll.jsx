73b7864ea8d7541c3d99919c1c78963deeb75da0
surlejonathan <surle.jonathan@gmail.com>
2023-04-23 12:17:02 +0200

    husky config 3

--- src/components/Scroll.jsx
+++ src/components/Scroll.jsx
@@ -1,16 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const Scroll = ({ children }) => {
-  var y = 'hello'
-  console.log(y)
   return (
     <div
-      style={{ height: '800px', paddingBottom: '50px' }}
+      style={{ height: "800px", paddingBottom: "50px" }}
       className="overflow-y-scroll ba bw3"
     >
       {children}
     </div>
-  )
-}
+  );
+};
 
-export default Scroll
+export default Scroll;
